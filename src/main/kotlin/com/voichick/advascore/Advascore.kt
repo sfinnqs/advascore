94471425be39222bf27510674ca5229744824c1a
sfinnqs <email@sfinnqs.org>
2019-09-28 08:48:13 -0500

    GPL

--- src/main/kotlin/com/voichick/advascore/Advascore.kt
+++ src/main/kotlin/com/voichick/advascore/Advascore.kt
@@ -1,3 +1,21 @@
+/**
+ * Advascore - A Bukkit plugin for scoring players by advancements
+ * Copyright (C) 2019 sfinnqs
+ *
+ * This file is part of Advascore.
+ *
+ * Advascore is free software; you can redistribute it and/or modify it under
+ * the terms of version 3 of the GNU General Public License as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <https://www.gnu.org/licenses>.
+ */
 package com.voichick.advascore
 
 import org.bstats.bukkit.Metrics
